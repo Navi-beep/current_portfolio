d614cb51170849022ee0a2aa66f6ddb0c029921a
Navi-Beep <estawars30@gmail.com>
2023-01-11 21:28:52 -0600

    adding bs javascript button

---
+++ main.js
@@ -0,0 +1,24 @@
+//let entry = prompt("Say hello there");
+//if (entry == null || entry == "") {
+ ///   alert("User left the chat.");
+///} else {
+ //alert("Hello there General Kenobi!");
+//}
+
+
+//var button = document.querySelector('button');
+
+//button.onmouseover = 
+//function changeColor() {
+//    button.style.background = 'black';
+    
+//}
+
+let x = 0
+let y = 0
+let z = 0 
+
+function myOverFunction() {
+    document.getElementById("demo3").innerHTML = y+=1;
+  }
+
