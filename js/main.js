928ba92a27511f7a410f346753bcdb3cd373a360
Navi-Beep <estawars30@gmail.com>
2023-01-25 21:16:13 -0600

    added function

--- js/main.js
+++ js/main.js
@@ -23,3 +23,27 @@ function myOverFunction() {
   }
 
 
+//array sum
+
+function sum41(arr1,arr2) {
+
+}
+
+sum41([1,2,3],[3,4,5])
+
+function sum42(arr1,arr2) {
+  let sumOne = 0 
+  for(let i =0; i < arr1.length; i += 1) {
+    sumOne += arr1[i]
+  }
+
+  let sumTwo = 0 
+  for(let i =0; i < arr2.length; i += 1) {
+    sumTwo += arr2[i]
+  }
+
+  return sumOne + sumTwo
+
+}
+
+console.log(sum42([5,2,3,4],[1,2,3]))
