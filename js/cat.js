db4961037a59a44d0d385e02c1c0ce60dfdc526c
Navi-Beep <estawars30@gmail.com>
2023-01-31 19:20:31 -0600

    made some changes to cat.html

---
+++ js/cat.js
@@ -0,0 +1,42 @@
+
+let gen_btn = document.querySelector('.gen_btn')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+
+    //const catFact = []
+              
+            //  async function getCatData(){
+           //   let res =  await fetch(`https://catfact.ninja/fact?max_length=100`)
+           //   let data =  await res.json();
+           //   const toebeans = data;
+           //   console.log(toebeans)
+
+           //   return toebeans;
+            
+           //   };
+
+           //   getCatData();
+
+
+
+
+            //console.log(catFact)
+
+}
+
+
+
+
